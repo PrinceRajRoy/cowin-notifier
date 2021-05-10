4db8893ff9c111b870d6edb8cc934a223784332d
Prince Raj Roy <princerajroy313@gmail.com>
2021-05-10 15:08:46 +0530

    Update: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import GithubIcon from "../../icons/GithubIcon";
+import { toggleModal } from "../../store/reducers";
 
 interface HeaderProps {
   title: string;
@@ -7,13 +10,34 @@ interface HeaderProps {
 
 const Header: React.FunctionComponent<HeaderProps> = ({
   title = "",
-  subtitle = ""
+  subtitle = "",
 }) => {
-  
+  const dispatch = useDispatch();
+  const handleToggle = () => {
+    dispatch(toggleModal(""));
+  };
+
   return (
-    <header>
-      <h2 className='pt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>{title}</h2>
-      <h1 className='pt-3 text-gray-900 sm:text-2xl'>{subtitle}</h1>
+    <header className="relative">
+      <h2 className="pt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        {title}
+      </h2>
+      <h1 className="pt-3 text-gray-900 sm:text-2xl">{subtitle}</h1>
+      <div className="absolute flex items-center right-5 gap-5 top-5">
+        <div
+          className="cursor-pointer bg-blue-100 rounded-md p-2 shadow"
+          onClick={handleToggle}
+        >
+          How To Use
+        </div>
+        <a
+          href="https://github.com/PrinceRajRoy/cowin-notifier"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GithubIcon />
+        </a>
+      </div>
     </header>
   );
 };
